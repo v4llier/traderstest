a4e937df9c92bdaf5be2708ac6bb760025d889eb
Vallier Adam <vallieradam@gmail.com>
2016-08-20 12:41:17 +0300

    adding decimal numbers & percentage right"

--- controller.rb
+++ controller.rb
@@ -6,8 +6,10 @@ end
 def result(num1, num2)
   if @operation == :/
     @answer = num1/num2
+    @answer.round(3)
   else
     @answer = num1.send(@operation, num2)
+    @answer.round(3)
   end
 end
 
@@ -20,10 +22,10 @@ def calculation
     @num2 = @dividable_numbers[0]
     result(@num1,@num2)
   elsif @operation == :*
-    numbers_multiplier
+    decimal_numbers_choice
     result(@num1,@num2)
   else
-    numbers
+    decimal_numbers_choice
     result(@num1,@num2)
   end
 end
@@ -33,14 +35,64 @@ def dividable_pool
   @pairs = (2..mx/2).flat_map { |n| (2..mx/n).map { |m| [n, n*m] } }
 end
 
-def numbers_multiplier
-  @num1 = rand(1..100)
-  @num2 = rand(1..25)
+def decimal_numbers_choice
+  numbers
+  if @operation == :*
+    @pairs = [
+      [@num_cent_a,@num_cent_b],
+      [@num_cent_a,@num_deca_a],
+      [@num_cent_b,@num_deci_a],
+      [@num_cent_b,@num_deci_lg],
+      [@num_deca_b,@num_deci_b],
+      [@num_deca_a,@num_deca_b],
+      [@num_unit_a,@num_centi],
+      [@num_unit_b,@num_centi_lg],
+      [@num_unit_a,@num_deci_lg]
+    ]
+    @pairs_choice = @pairs.sample
+    @num1 = @pairs_choice[0]
+    @num2 = @pairs_choice[1]
+  else
+    @num1 = @numbers.sample
+    @num2 = @numbers.sample
+    until @num1 != @num2
+      @num2 = @numbers.sample
+    end
+  end
 end
+# def numbers_multiplier
+#   @num1 = rand(1..100)
+#   @num2 = rand(1..25)
+# end
 
 def numbers
-  @num1 = rand(5..100)
-  @num2 = rand(5..100)
+  @num_cent_a = rand(5..100)
+  @num_cent_b = rand(5..100)
+  @num_unit_a = rand(2..10)
+  @num_unit_b = rand(2..10)
+  @num_deca_a = rand(1..50)
+  @num_deca_b = rand(1..50)
+  @num_deci_lg = rand(0.1..50).round(1)
+  @num_deci_a = rand(0.1..0.9).round(1)
+  @num_deci_b = rand(0.1..0.9).round(1)
+  @num_milli_lg = rand(0.001..15).round(3)
+  @num_centi_lg = rand(0.01..30).round(2)
+  @num_centi = rand(0.01..0.99).round(2)
+
+  @numbers = [
+  @num_cent_a,
+  @num_cent_b,
+  @num_unit_a,
+  @num_unit_b,
+  @num_deca_a,
+  @num_deca_b,
+  @num_deci_lg,
+  @num_deci_a,
+  @num_deci_b,
+  @num_milli_lg,
+  @num_centi_lg,
+  @num_centi,
+  ]
 end
 
 
@@ -162,8 +214,10 @@ def program
     puts puts
 
   end
-  @percentage = @right/@total*100
-  puts "You got #{@right} answer(s) (#{@percentage}%)"
+  @right
+  @percentage = (@right.fdiv(@total) * 100).round(0)
+
+  puts "You got #{@right} answer(s) right (#{@percentage}%)"
 end
 
 program
